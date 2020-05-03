5ba552b27869ad19b569d3f289ef90bdef33e655
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-03 00:36:46 -0300

    feat: add spinner and fix cep validation

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,6 @@ function scrollTo(element) {
 }
 
 
-
 function checkRecaptcha() {
   var response = grecaptcha.getResponse();
   if(response.length == 0) { 
@@ -20,6 +19,16 @@ function checkRecaptcha() {
   }
 }
 
+
+function showSpinner() {
+  document.getElementById("cep-spinner").classList.remove("hide-spinner");
+}
+
+function hideSpinner() {
+  document.getElementById("cep-spinner").classList.add("hide-spinner");
+}
+
+
 function afterPageLoad() {
   /* Button scroll event handler*/
   document.getElementById("cta-button").addEventListener('click', () => {
@@ -28,24 +37,43 @@ function afterPageLoad() {
 
   /* CEP event handler*/
   const cepElement = document.getElementById("cep");
-  cepElement.addEventListener("focusout", function () {
-    fetch(`https://viacep.com.br/ws/${cepElement.value}/json/unicode/`, { method: 'GET', mode: 'cors' })
+
+  cepElement.addEventListener("keyup", function () {
+    const enterIsPressed = event.key !== "Enter";
+    const isNotValidCEP = cepElement.value.length !== 8;
+    if (enterIsPressed) {
+      return;
+    };
+    if (isNotValidCEP) {
+      alert("O CEP deve conter 8 dígitos")
+      return;
+    }
+
+    showSpinner();
+    const myHeaders = new Headers();
+    const myInit = {
+      headers: myHeaders,
+      method: 'GET',
+      mode: 'cors'
+    }
+    fetch(`https://viacep.com.br/ws/${cepElement.value}/json/unicode/`, myInit)
       .then( (response) => {
-          if (response.status === 400) {
-            console.alert('CEP inválido');
-            return;
-          }
-          else if (response.status !== 200) {
-            console.alert('Algo deu errado ao buscar o CEP, por favor tente novamente');
-            return;
-          }
+          hideSpinner();
           return response.json();
       })
       .then((cepData) => {
-        document.getElementById("logradouro").value = cepData.logradouro;
-        document.getElementById("bairro").value = cepData.bairro;
-        document.getElementById("cidade").value = cepData.localidade;
-        document.getElementById("uf").value = cepData.uf;
+        if (cepData.erro) {
+          alert('CEP não encontrado');
+        } else {
+          document.getElementById("logradouro").value = cepData.logradouro;
+          document.getElementById("bairro").value = cepData.bairro;
+          document.getElementById("cidade").value = cepData.localidade;
+          document.getElementById("uf").value = cepData.uf;
+        }
+      })
+      .catch(() => {
+        hideSpinner();
+        alert('Houve um erro na validação do CEP');
       });
   });
 }
