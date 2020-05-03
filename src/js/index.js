5d0b4bd177811622a75c9eafc06f6a08e96eba0f
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-03 14:21:42 -0300

    feat: add debounce and maxlength to CEP field

--- src/js/index.js
+++ src/js/index.js
@@ -28,6 +28,59 @@ function hideSpinner() {
   document.getElementById("cep-spinner").classList.add("hide-spinner");
 }
 
+function debounce(callback, wait) {
+  let timeout = null;
+
+  return function() {
+    const callNow = !timeout;
+    const next = () => callback.apply(this, arguments);
+    
+    clearTimeout(timeout)
+    timeout = setTimeout(next, wait);
+
+    if (callNow) {
+      next();
+    }
+  }
+}
+
+
+function callCepAPI(cepElement, warnElement) {
+  const isNotValidCEP = cepElement.value.length !== 8;
+  if (isNotValidCEP) {
+    warnElement.textContent = 'O CEP deve conter 8 dígitos';
+    return;
+  }
+
+  showSpinner();
+  warnElement.textContent = '';
+  const myHeaders = new Headers();
+  const myInit = {
+    headers: myHeaders,
+    method: 'GET',
+    mode: 'cors'
+  }
+  fetch(`https://viacep.com.br/ws/${cepElement.value}/json/unicode/`, myInit)
+    .then( (response) => {
+        hideSpinner();
+        return response.json();
+    })
+    .then((cepData) => {
+      if (cepData.erro) {
+        warnElement.textContent = 'CEP não encontrado';
+      } else {
+        document.getElementById("logradouro").value = cepData.logradouro;
+        document.getElementById("bairro").value = cepData.bairro;
+        document.getElementById("cidade").value = cepData.localidade;
+        document.getElementById("uf").value = cepData.uf;
+      }
+    })
+    .catch(() => {
+      hideSpinner();
+      warnElement.textContent = 'Houve um erro na validação do CEP';
+    });
+};
+
 
 function afterPageLoad() {
   /* Button scroll event handler*/
@@ -35,46 +88,11 @@ function afterPageLoad() {
     scrollTo(document.getElementById("covid-form-container"));
   });
 
-  /* CEP event handler*/
   const cepElement = document.getElementById("cep");
-
-  cepElement.addEventListener("keyup", function () {
-    const enterIsPressed = event.key !== "Enter";
-    const isNotValidCEP = cepElement.value.length !== 8;
-    if (enterIsPressed) {
-      return;
-    };
-    if (isNotValidCEP) {
-      alert("O CEP deve conter 8 dígitos")
-      return;
-    }
-
-    showSpinner();
-    const myHeaders = new Headers();
-    const myInit = {
-      headers: myHeaders,
-      method: 'GET',
-      mode: 'cors'
-    }
-    fetch(`https://viacep.com.br/ws/${cepElement.value}/json/unicode/`, myInit)
-      .then( (response) => {
-          hideSpinner();
-          return response.json();
-      })
-      .then((cepData) => {
-        if (cepData.erro) {
-          alert('CEP não encontrado');
-        } else {
-          document.getElementById("logradouro").value = cepData.logradouro;
-          document.getElementById("bairro").value = cepData.bairro;
-          document.getElementById("cidade").value = cepData.localidade;
-          document.getElementById("uf").value = cepData.uf;
-        }
-      })
-      .catch(() => {
-        hideSpinner();
-        alert('Houve um erro na validação do CEP');
-      });
+  const warnElement = document.getElementById("cep-warning");
+  const cepCallback = debounce(() => callCepAPI(cepElement, warnElement), 700);
+  document.getElementById("cep").addEventListener('input', () => {
+    cepCallback();
   });
 }
 
