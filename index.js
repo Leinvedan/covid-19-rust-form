78ca060dc80e1a47564f3f9a2673d9cf34ed5232
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-03 19:45:08 -0300

    remove unused variables

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
-
 const PORT = 5500;
 
 app.use(express.static('dist'));
