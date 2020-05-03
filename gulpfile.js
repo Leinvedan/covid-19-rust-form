9cb8bac9cd98487970884d2b8cdff993e100d7d9
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-02 21:20:06 -0300

    feat: add gulp to minify

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+
+const DEST_DIR = "./dist";
+const SOURCE = "./src"
+
+
+function minify_html() {
+  return gulp.src(`${SOURCE}/*.html`)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(DEST_DIR));
+};
+
+function minify_css() {
+  return gulp.src(`${SOURCE}/css/*.css`)
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(DEST_DIR));
+};
+
+function minify_js () {
+  return gulp.src(`${SOURCE}/js/*.js`)
+    .pipe(minify())
+    .pipe(gulp.dest(DEST_DIR))
+};
+
+exports.default = gulp.series(minify_html, minify_css, minify_js);
