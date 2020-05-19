c33ee79acee04a3e8e016eec3888625af4588a79
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-19 02:04:45 -0300

    feat: inject critical css in HTML

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,31 @@ const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const minify = require('gulp-minify');
+const inject = require('gulp-inject');
 
 const DEST_DIR = "./dist";
 const SOURCE = "./src"
 
 
 function minify_html() {
-  return gulp.src(`${SOURCE}/*.html`)
+  let target = gulp.src(`${SOURCE}/*.html`)
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest(DEST_DIR));
+  let critical_css = gulp.src(`${SOURCE}/css/critical/*.css`)
+    .pipe(concat('critical.min.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+  return target
+      .pipe(inject(critical_css, {
+          transform: function(_, file) {
+            return `<style>${file.contents.toString()}</style>`
+          }
+        }
+      ))
+      .pipe(gulp.dest(DEST_DIR));
 };
 
 function minify_css() {
   return gulp.src(`${SOURCE}/css/*.css`)
     .pipe(concat('style.min.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(DEST_DIR));
-};
-
-function minify_css_critical() {
-  return gulp.src(`${SOURCE}/css/critical/*.css`)
-    .pipe(concat('critical.min.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest(DEST_DIR));
 };
@@ -35,5 +39,5 @@ function minify_js () {
     .pipe(gulp.dest(DEST_DIR))
 };
 
-exports.default = gulp.parallel(minify_html, minify_css, minify_js, minify_css_critical);
-exports.watch = () => {gulp.watch(SOURCE, gulp.parallel(minify_html, minify_css, minify_js, minify_css_critical))};
+exports.default = gulp.parallel(minify_html, minify_css, minify_js);
+exports.watch = () => {gulp.watch(SOURCE, gulp.parallel(minify_html, minify_css, minify_js))};
