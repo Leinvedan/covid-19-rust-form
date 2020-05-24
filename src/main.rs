a03b92db9c0eaa99c3678cb159da562acb0d3eff
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-24 18:30:38 -0300

    refactor: use map_err instead of let if

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,55 @@
 use std::env;
-use actix_web::{ post, App, HttpServer, middleware, client::Client, HttpResponse, web };
+use actix_web::{ post, App, HttpServer, middleware, client::Client, HttpResponse, web, Error };
 use serde_qs::Config;
 use diesel::mysql::MysqlConnection;
 use covid_survey::models::{ CaptchaResponse, Body, FormParameters, EnvData };
 use covid_survey::database::{ get_db_connection, insert_form_data };
+// use log::{info, warn, error};
 
+
+fn log_and_return_http<T>(_: T, log: &str) -> HttpResponse {
+    println!("{}", log);
+    HttpResponse::InternalServerError().body("500 Internal error")
+}
 
 
 #[post("/validate")]
-async fn index(payload: String, env_data: web::Data<EnvData>) -> HttpResponse {
+async fn index(payload: String, env_data: web::Data<EnvData>) -> Result<HttpResponse, Error> {
 
     let config = Config::new(10, false);
     let deserialized_params: Result<FormParameters, _> = config.deserialize_str(&payload);
     println!("payload={}", payload);
-    if let Ok(deserialized_form) = deserialized_params {
+    let deserialized_form = deserialized_params
+        .map_err(|err| log_and_return_http(err, "Unable to desserialize"))?;
 
-        println!("\n\nparams: {:?}", deserialized_form);
-        let recaptcha_response: String = deserialized_form.recaptcha_response.clone();
+    println!("\n\nparams: {:?}", deserialized_form);
+    let recaptcha_response: String = deserialized_form.recaptcha_response.clone();
 
-        let request_body = Body::new(env_data.captcha_secret.clone(), recaptcha_response);
-        let client = Client::default();
+    let request_body = Body::new(env_data.captcha_secret.clone(), recaptcha_response);
+    let client = Client::default();
 
-        if let Ok(request_client) = client
-            .post("https://www.google.com/recaptcha/api/siteverify")
-            .content_type("application/x-www-form-urlencoded")
-            .query(&request_body)
-            {
-                let response = request_client.send().await;
-                println!("requestBody={:?}", request_body);
-                if let Ok(mut data) = response {
-                    if let Ok(parsed_data) = data.json::<CaptchaResponse>().await {
-                        println!("SCORE = {}", parsed_data.score.unwrap_or(0.0));
-                        println!("captchaData={:?}", parsed_data);
-                        if parsed_data.score.unwrap_or(0.0) >= 0.5 {
-                            let inserted: bool = insert_form_data(&env_data.db_conn, deserialized_form);
-                            println!("INSETED? = {}", inserted);
-                        }
-
-                        return HttpResponse::Ok().body(
-                            "<h1>Obrigado por participar!</br>Sua ajuda foi fundamental no combate ao COVID-19!</h1>"
-                        );
-                    } else {
-                        println!("Error parsing Google API Response");
-                    }
-                } else {
-                    println!("Error trying to POST Google API");
-                }
-            } else {
-                println!("Error parsing body to query");
-            }
-    } else {
-        println!("Unable do desserialize data");
+    let request_client = client
+      .post("https://www.google.com/recaptcha/api/siteverify")
+      .content_type("application/x-www-form-urlencoded")
+      .query(&request_body)
+      .map_err(|err| log_and_return_http(err, "Unable to parse Body into query"))?;
+    let mut response = request_client
+        .send()
+        .await
+        .map_err(|err| log_and_return_http(err, "Unable connect to Google API"))?;
+    println!("requestBody={:?}", request_body);
+    let parsed_data = response.json::<CaptchaResponse>()
+        .await
+        .map_err(|err| log_and_return_http(err, "Unable to parse Google API Response"))?;
+    println!("SCORE = {}", parsed_data.score.unwrap_or(0.0));
+    println!("captchaData={:?}", parsed_data);
+    if parsed_data.score.unwrap_or(0.0) >= 0.5 {
+        let inserted: bool = insert_form_data(&env_data.db_conn, deserialized_form);
+        println!("INSETED? = {}", inserted);
     }
-    HttpResponse::InternalServerError().body("500 Internal error")
+
+    Ok(HttpResponse::Ok().body(
+        "<h1>Obrigado por participar!</br>Sua ajuda foi fundamental no combate ao COVID-19!</h1>"))
 }
 
 
