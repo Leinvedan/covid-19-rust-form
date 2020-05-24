5e29f6e6e935c8e637d6658666c7477f07c3ab77
Leinvedan <fontebasso.lemos@gmail.com>
2020-05-24 19:59:04 -0300

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use serde_qs::Config;
 use diesel::mysql::MysqlConnection;
 use covid_survey::models::{ CaptchaResponse, Body, FormParameters, EnvData };
 use covid_survey::database::{ get_db_connection, insert_form_data };
-// use log::{info, warn, error};
+use log::{ info, error };
+use env_logger::Env;
 
 
 fn log_and_return_http<T>(_: T, log: &str) -> HttpResponse {
-    println!("{}", log);
+    error!("{}", log);
     HttpResponse::InternalServerError().body("500 Internal error")
 }
 
@@ -18,11 +19,9 @@ async fn index(payload: String, env_data: web::Data<EnvData>) -> Result<HttpResp
 
     let config = Config::new(10, false);
     let deserialized_params: Result<FormParameters, _> = config.deserialize_str(&payload);
-    println!("payload={}", payload);
     let deserialized_form = deserialized_params
         .map_err(|err| log_and_return_http(err, "Unable to desserialize"))?;
 
-    println!("\n\nparams: {:?}", deserialized_form);
     let recaptcha_response: String = deserialized_form.recaptcha_response.clone();
 
     let request_body = Body::new(env_data.captcha_secret.clone(), recaptcha_response);
@@ -37,15 +36,13 @@ async fn index(payload: String, env_data: web::Data<EnvData>) -> Result<HttpResp
         .send()
         .await
         .map_err(|err| log_and_return_http(err, "Unable connect to Google API"))?;
-    println!("requestBody={:?}", request_body);
     let parsed_data = response.json::<CaptchaResponse>()
         .await
         .map_err(|err| log_and_return_http(err, "Unable to parse Google API Response"))?;
-    println!("SCORE = {}", parsed_data.score.unwrap_or(0.0));
-    println!("captchaData={:?}", parsed_data);
+    info!("SCORE={}", parsed_data.score.unwrap_or(0.0));
     if parsed_data.score.unwrap_or(0.0) >= 0.5 {
         let inserted: bool = insert_form_data(&env_data.db_conn, deserialized_form);
-        println!("INSETED? = {}", inserted);
+        info!("INSERTED={}", inserted);
     }
 
     Ok(HttpResponse::Ok().body(
@@ -58,7 +55,7 @@ fn build_env_data() -> EnvData {
     let default_secret = String::from("myLittleSecret");
     let captcha_secret = env::var("CAPTCHA_SECRET").unwrap_or(default_secret);
     let db_conn: MysqlConnection = get_db_connection();
-    println!("Connected to database");
+    info!("Connected to database");
     EnvData {
         captcha_secret,
         db_conn
@@ -68,17 +65,21 @@ fn build_env_data() -> EnvData {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let log_env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "info")
+        .write_style_or("MY_LOG_STYLE", "always");
+    env_logger::init_from_env(log_env);
+
     let default_port = String::from("8080");
     let app_port = env::var("PORT").unwrap_or(default_port);
     let addr = format!("0.0.0.0:{}", app_port);
 
-    std::env::set_var("RUST_LOG", "actix_web=info");
     let server = HttpServer::new(|| { App::new()
             .data(build_env_data())
             .wrap(middleware::Logger::default())
             .service(index)
         })
         .bind(&addr)?;
-    println!("Starting http server: {}", &addr);
+    info!("Starting http server: {}", &addr);
     server.run().await
 }
